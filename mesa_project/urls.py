88a9b21f881e68a774f4f7626aa6387453bcfe1d
Soportecarb <carb.graciev@gmail.com>
2025-11-27 12:27:26 -0500

    Delete mesa_project directory

--- mesa_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # Conecta todas las rutas de la app mesa
-    path("", include("mesa.urls")),
-]
